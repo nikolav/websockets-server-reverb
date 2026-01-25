105686acb33af9a09b243acb7e3dae7ea8992bae
Vukovic Nikola <admin@nikolav.rs>
2026-01-25 15:26:52 +0100

    Sun, Jan 25, 2026  2:26:52 PM

--- docker/postgres-start.sh
+++ docker/postgres-start.sh
@@ -3,12 +3,15 @@ set -eu
 
 DATA_DIR="/var/lib/postgresql/data"
 LOG_DIR="/var/lib/postgresql/log"
+RUN_DIR="/run/postgresql"
 
 # ensure directories exist, ownership correct
-mkdir -p "$DATA_DIR" "$LOG_DIR"
+mkdir -p "$DATA_DIR" "$LOG_DIR" "$RUN_DIR"
 chown -R postgres:postgres /var/lib/postgresql
+chown -R postgres:postgres /run/postgresql
 
 # run as postgres user
 exec gosu postgres /usr/bin/postgres \
   -D "$DATA_DIR" \
-  -c listen_addresses=127.0.0.1
+  -c listen_addresses=127.0.0.1 \
+  -c unix_socket_directories=/run/postgresql
