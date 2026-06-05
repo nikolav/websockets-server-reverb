a8d11004ec85411daa842aea1fa181315a198361
Vukovic Nikola <admin@nikolav.rs>
2026-06-06 01:29:41 +0200

    Fri, Jun  5, 2026 11:29:41 PM

--- config/logging.php
+++ config/logging.php
@@ -65,9 +65,16 @@ return [
             'replace_placeholders' => true,
         ],
 
+        // 'daily' => [
+        //     'driver' => 'daily',
+        //     'path' => storage_path('logs/laravel.log'),
+        //     'level' => env('LOG_LEVEL', 'debug'),
+        //     'days' => env('LOG_DAILY_DAYS', 14),
+        //     'replace_placeholders' => true,
+        // ],
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/daily.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => env('LOG_DAILY_DAYS', 14),
             'replace_placeholders' => true,
@@ -89,7 +96,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
